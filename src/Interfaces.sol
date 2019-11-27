ba3e7591f02b267da49e3d553d58986c0a3ffe0d
Brian McMichael <brian@brianmcmichael.com>
2019-11-27 15:39:10 -0500

    Add Flap Abstract

--- src/Interfaces.sol
+++ src/Interfaces.sol
@@ -4,3 +4,4 @@ import { VatAbstract } from "./dss/VatAbstract.sol";
 import { PotAbstract } from "./dss/PotAbstract.sol";
 import { DaiAbstract } from "./dss/DaiAbstract.sol";
 import { GemAbstract } from "./dss/GemAbstract.sol";
+import { FlapAbstract } from "./dss/FlapAbstract.sol";
---
+++ src/dss/FlapAbstract.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.5.12;
+
+import { VatAbstract } from "./VatAbstract.sol";
+import { GemAbstract } from "./GemAbstract.sol";
+
+// https://github.com/makerdao/dss/blob/master/src/flap.sol
+contract FlapAbstract {
+    mapping (address => uint256) public wards;
+    function rely(address usr) external;
+    function deny(address usr) external;
+    struct Bid {
+        uint256 bid;
+        uint256 lot;
+        address guy;  // high bidder
+        uint48  tic;  // expiry time
+        uint48  end;
+    }
+    mapping (uint256 => Bid) public bids;
+    VatAbstract public vat;
+    GemAbstract public gem;
+    uint256 public ONE;
+    uint256 public beg;
+    uint48 public ttl;
+    uint48 public tau;
+    uint256 public kicks;
+    uint256 public live;
+    event Kick(uint256, uint256, uint256);
+    function file(bytes32, uint256) external;
+    function kick(uint256, uint256) external returns (uint256);
+    function tick(uint256) external;
+    function tend(uint256, uint256, uint256) external;
+    function deal(uint256) external;
+    function cage(uint256) external;
+    function yank(uint256) external;
+}
