25f63b96aa73dc2746695d74d65a04092f9febcd
Brian McMichael <brian@brianmcmichael.com>
2019-11-27 18:05:08 -0500

    Make GemAbstract conform to base EIP20

---
+++ src/ERC/GemAbstract.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.12;
+
+// A base ERC-20 abstract class
+// https://eips.ethereum.org/EIPS/eip-20
+contract GemAbstract {
+    function totalSupply() public view returns (uint256);
+    function balanceOf(address) public view returns (uint256);
+    function allowance(address, address) public view returns (uint256);
+    function approve(address, uint256) public returns (bool);
+    function transfer(address, uint256) public returns (bool);
+    function transferFrom(address, address, uint256) public returns (bool);
+    event Approval(address, address, uint256);
+    event Transfer(address, address, uint256);
+}
