9750b435a8010debc174b7641eaab34dcd014ed1
Brian McMichael <brian@brianmcmichael.com>
2019-11-27 15:26:51 -0500

    Add Gem interface

--- src/Interfaces.sol
+++ src/Interfaces.sol
@@ -3,3 +3,4 @@ pragma solidity ^0.5.12;
 import { VatAbstract } from "./dss/VatAbstract.sol";
 import { PotAbstract } from "./dss/PotAbstract.sol";
 import { DaiAbstract } from "./dss/DaiAbstract.sol";
+import { GemAbstract } from "./dss/GemAbstract.sol";
