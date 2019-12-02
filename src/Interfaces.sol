c98ab9078716e5a7ebc8423377d61a2a4c144671
Brian McMichael <brian@brianmcmichael.com>
2019-12-02 16:17:22 -0500

    Add all new interfaces

--- src/Interfaces.sol
+++ src/Interfaces.sol
@@ -1,19 +1,26 @@
 pragma solidity ^0.5.12;
 
 import { GemAbstract } from "./ERC/GemAbstract.sol";
-import { TokenAbstract } from "./dapp/TokenAbstract.sol";
+
+import { DSAuthorityAbstract, DSAuthEventsAbstract, DSAuthAbstract } from "./dapp/AuthorityAbstract.sol";
+import { DSChiefAbstract } from "./dapp/ChiefAbstract.sol";
+import { PauseAbstract } from "./dapp/PauseAbstract.sol";
+import { PauseProxyAbstract } from "./dapp/PauseProxyAbstract.sol";
 import { PipAbstract } from "./dapp/PipAbstract.sol";
-import { VatAbstract } from "./dss/VatAbstract.sol";
+import { RolesAbstract } from "./dapp/RolesAbstract.sol";
+import { TokenAbstract } from "./dapp/TokenAbstract.sol";
+
+import { AuthGemJoinAbstract } from "./dss/AuthGemJoinAbstract.sol";
+import { CatAbstract } from "./dss/CatAbstract.sol";
+import { DaiAbstract } from "./dss/DaiAbstract.sol";
+import { DaiJoinAbstract } from "./dss/DaiJoinAbstract.sol";
+import { EndAbstract } from "./dss/EndAbstract.sol";
+import { ETHJoinAbstract } from "./dss/ETHJoinAbstract.sol";
+import { FlapAbstract } from "./dss/FlapAbstract.sol";
+import { FlipAbstract } from "./dss/FlipAbstract.sol";
+import { FlopAbstract } from "./dss/FlopAbstract.sol";
+import { GemJoinAbstract } from "./dss/GemJoinAbstract.sol";
 import { PotAbstract } from "./dss/PotAbstract.sol";
 import { SpotAbstract } from "./dss/SpotAbstract.sol";
-import { DaiAbstract } from "./dss/DaiAbstract.sol";
-import { FlapAbstract } from "./dss/FlapAbstract.sol";
-import { FlopAbstract } from "./dss/FlopAbstract.sol";
-import { FlipAbstract } from "./dss/FlipAbstract.sol";
+import { VatAbstract } from "./dss/VatAbstract.sol";
 import { VowAbstract } from "./dss/VowAbstract.sol";
-import { CatAbstract } from "./dss/CatAbstract.sol";
-import { EndAbstract } from "./dss/EndAbstract.sol";
-import { GemJoinAbstract } from "./dss/GemJoinAbstract.sol";
-import { ETHJoinAbstract } from "./dss/ETHJoinAbstract.sol";
-import { DaiJoinAbstract } from "./dss/DaiJoinAbstract.sol";
-import { AuthGemJoinAbstract } from "./dss/AuthGemJoinAbstract.sol";
