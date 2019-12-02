8d1ec25034128a5338e54bb3f310d721af3845b0
Brian McMichael <brian@brianmcmichael.com>
2019-12-02 13:14:32 -0500

    Add pause proxy abstract

---
+++ src/dapp/PauseProxyAbstract.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.12;
+
+// https://github.com/dapphub/ds-pause
+contract PauseProxyAbstract {
+    // address public owner;
+    function owner() public returns (address);
+    function exec(address, bytes memory) public returns (bytes memory);
+}
