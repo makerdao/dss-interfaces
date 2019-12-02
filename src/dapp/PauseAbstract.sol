7ef5238617e10e3ddbde9a039809925ed225a215
Brian McMichael <brian@brianmcmichael.com>
2019-12-02 13:14:13 -0500

    Add pause abstract

---
+++ src/dapp/PauseAbstract.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.5.12;
+
+import "./PauseProxyAbstract.sol"
+
+// https://github.com/dapphub/ds-pause
+contract PauseAbstract {
+    function setOwner(address) public;
+    function setAuthority(DSAuthority authority_) public;
+    function setDelay(uint256) public;
+    // mapping (bytes32 => bool) public plans;
+    function plans(bytes32) public returns (bool);
+    // DSProxyAbstract public proxy;
+    function proxy() public returns (PauseProxyAbstract)
+    // uint256 public delay;
+    function delay() public returns (uint256);
+    function plot(address, bytes32, bytes memory, uint256) public;
+    function drop(address, bytes32, bytes memory, uint256) public;
+    function exec(address, bytes32, bytes memory, uint256) public returns (bytes memory);
+}
