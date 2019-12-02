2e91ef4828dfa6f8fcaad5a8c6e75d0f3b12e380
Brian McMichael <brian@brianmcmichael.com>
2019-12-02 16:45:02 -0500

    Add a MKR token alias

---
+++ src/dss/MKRAbstract.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.5.12;
+
+import { DSTokenAbstract } from "../dapp/DSTokenAbstract.sol";
+
+// MKR Token adheres to the DS-Token contract interface
+contract MKRAbstract is DSTokenAbstract {}
