f5ec21e00f77cd93e0ee0b0a0be94b02c25ecd79
Brian McMichael <brian@brianmcmichael.com>
2019-12-10 17:12:30 -0500

    Convert abstract public vars to view

--- src/dss/PotAbstract.sol
+++ src/dss/PotAbstract.sol
@@ -5,25 +5,25 @@ import { VatAbstract } from "./VatAbstract.sol";
 // https://github.com/makerdao/dss/blob/master/src/pot.sol
 contract PotAbstract {
     // mapping (address => uint256) public wards;
-    function wards(address) public returns (uint256);
+    function wards(address) public view returns (uint256);
     function rely(address) external;
     function deny(address) external;
     // mapping (address => uint256) public pie;  // user Savings Dai
-    function pie(address) public returns (uint256);
+    function pie(address) public view returns (uint256);
     // uint256 public Pie;  // total Savings Dai
-    function Pie() public returns (uint256);
+    function Pie() public view returns (uint256);
     // uint256 public dsr;  // the Dai Savings Rate
-    function dsr() public returns (uint256);
+    function dsr() public view returns (uint256);
     // uint256 public chi;  // the Rate Accumulator
-    function chi() public returns (uint256);
+    function chi() public view returns (uint256);
     // VatAbstract public vat;  // CDP engine
-    function vat() public returns (VatAbstract);
+    function vat() public view returns (VatAbstract);
     // address public vow;  // debt engine
-    function vow() public returns (address);
+    function vow() public view returns (address);
     // uint256 public rho;  // time of last drip
-    function rho() public returns (uint256);
+    function rho() public view returns (uint256);
     // uint256 public live;  // Access Flag
-    function live() public returns (uint256);
+    function live() public view returns (uint256);
     function file(bytes32, uint256) external;
     function file(bytes32, address) external;
     function cage() external;
