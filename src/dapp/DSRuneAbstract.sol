30d2bb6656b6e0e4dc7a74b16328f1104ce0897a
Brian McMichael <brian@brianmcmichael.com>
2020-01-23 17:21:10 -0500

    rename fax to sig

--- src/dapp/DSRuneAbstract.sol
+++ src/dapp/DSRuneAbstract.sol
@@ -5,7 +5,7 @@ contract DSRuneAbstract {
     function pause()    public view returns (address);
     function usr()      public view returns (address);
     function tag()      public view returns (bytes32);
-    function fax()      public view returns (bytes32);
+    function sig()      public view returns (bytes32);
     function eta()      public view returns (uint256);
     function schedule() public;
     function done()     public view returns (bool);
