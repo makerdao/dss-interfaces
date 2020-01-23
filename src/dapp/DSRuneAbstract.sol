9bf5e7aefaf9c653f3feb22225ec44fcaffd897f
Brian McMichael <brian@brianmcmichael.com>
2020-01-23 17:56:54 -0500

    Add some function comments

--- src/dapp/DSRuneAbstract.sol
+++ src/dapp/DSRuneAbstract.sol
@@ -17,12 +17,21 @@ pragma solidity ^0.5.12;
 
 // https://github.com/makerdao/dss-spellbook
 contract DSRuneAbstract {
+    // @return [address] A contract address conforming to DSPauseAbstract
     function pause()    public view returns (address);
-    function usr()      public view returns (address);
+    // @return [address] The address of the contract to be executed
+    // TODO: is `action()` a required field? Not all spells rely on a seconary contract.
+    function action()   public view returns (address);
+    // @return [bytes32] extcodehash of rune address
     function tag()      public view returns (bytes32);
+    // @return [bytes] The `abi.encodeWithSignature()` result of the function to be called.
     function sig()      public view returns (bytes);
+    // @return [uint256] Earliest time rune can execute
     function eta()      public view returns (uint256);
+    // The schedule() function plots the rune in the DSPause
     function schedule() public;
+    // @return [bool] true if the rune has been cast()
     function done()     public view returns (bool);
+    // The cast() function executes the rune
     function cast()     public;
 }
