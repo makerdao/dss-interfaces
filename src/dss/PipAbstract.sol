35b288bcb4aeb02d788f7e538e8cadf24f807122
Brian McMichael <brian@brianmcmichael.com>
2020-02-24 10:21:57 -0500

    Add pip and saitoken comments

--- src/dss/PipAbstract.sol
+++ src/dss/PipAbstract.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.5.12;
 import { DSValueAbstract } from "../dapp/DSValueAbstract.sol";
 
 // Pip is a DS-Value used within the DSS
+//  Consider using the DSValueAbstract directly when abstracting PIP
 contract PipAbstract is DSValueAbstract {}
