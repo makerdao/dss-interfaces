a01dc5581197a82696413597529c4c21beabdfcc
Brian McMichael <brian@brianmcmichael.com>
2020-02-24 10:12:55 -0500

    Comment on MKRAbstract

--- src/dss/MKRAbstract.sol
+++ src/dss/MKRAbstract.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.5.12;
 import { DSTokenAbstract } from "../dapp/DSTokenAbstract.sol";
 
 // MKR Token adheres to the DS-Token contract interface
+//  Consider using DSTokenAbstract directly when abstracting MKR
 contract MKRAbstract is DSTokenAbstract {}
