4222bf307f3aea40c92da595661d3bc55798fead
Christopher Mooney <chris@dod.net>
2020-07-10 15:15:18 -0400

    remove variable names

--- src/dss/IlkRegistryAbstract.sol
+++ src/dss/IlkRegistryAbstract.sol
@@ -3,48 +3,48 @@ pragma solidity ^0.5.12;
 // https://github.com/makerdao/ilk-registry/blob/master/src/IlkRegistry.sol
 contract IlkRegistryAbstract;
     function wards(address) public view returns (uint256);
-    function rely(address usr) external;
-    function deny(address usr) external;
+    function rely(address) external;
+    function deny(address) external;
     function vat() public view returns (address);
     function cat() public view returns (address);
     function spot() public view returns (address);
     function ilkData(bytes32) public view returns (
-        uint256 pos,
-        address gem,
-        address pip,
-        address join,
-        address flip,
-        uint256 dec,
-        string memory name,
+        uint256,
+        address,
+        address,
+        address,
+        address,
+        uint256,
+        string memory,
         string memory symbol
     );
     function ilks() public view returns (bytes32[] memory);
     function ilks(uint) public view returns (bytes32);
     function add(address _adapter) external;
-    function remove(bytes32 ilk) external;
-    function removeAuth(bytes32 ilk) external;
-    function file(bytes32 ilk, bytes32 what, address data) external;
-    function file(bytes32 ilk, bytes32 what, uint256 data) external;
-    function file(bytes32 ilk, bytes32 what, string calldata data) external;
+    function remove(bytes32) external;
+    function removeAuth(bytes32) external;
+    function file(bytes32, bytes32, address) external;
+    function file(bytes32, bytes32, uint256) external;
+    function file(bytes32, bytes32, string calldata) external;
     function count() external view returns (uint256);
     function list() external view returns (bytes32[] memory);
-    function list(uint256 start, uint256 end) external view returns (bytes32[] memory);
-    function get(uint256 pos) external view returns (bytes32);
-    function info(bytes32 ilk) external view returns (
-        string memory name,
-        string memory symbol,
-        uint256 dec,
-        address gem,
-        address pip,
-        address join,
-        address flip
+    function list(uint256, uint256) external view returns (bytes32[] memory);
+    function get(uint256) external view returns (bytes32);
+    function info(bytes32) external view returns (
+        string memory,
+        string memory,
+        uint256,
+        address,
+        address,
+        address,
+        address
     );
-    function pos(bytes32 ilk) external view returns (uint256);
-    function gem(bytes32 ilk) external view returns (address);
-    function pip(bytes32 ilk) external view returns (address);
-    function join(bytes32 ilk) external view returns (address);
-    function flip(bytes32 ilk) external view returns (address);
-    function dec(bytes32 ilk) external view returns (uint256);
-    function symbol(bytes32 ilk) external view returns (string memory);
-    function name(bytes32 ilk) external view returns (string memory);
+    function pos(bytes32) external view returns (uint256);
+    function gem(bytes32) external view returns (address);
+    function pip(bytes32) external view returns (address);
+    function join(bytes32) external view returns (address);
+    function flip(bytes32) external view returns (address);
+    function dec(bytes32) external view returns (uint256);
+    function symbol(bytes32) external view returns (string memory);
+    function name(bytes32) external view returns (string memory);
 }
