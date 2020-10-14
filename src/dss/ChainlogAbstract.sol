f7cfb3fb4ff4c34baeeaae8b14f8a569e9b139d9
Brian McMichael <brian@brianmcmichael.com>
2020-10-14 15:33:09 -0400

    Add chainlog

---
+++ src/dss/ChainlogAbstract.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.5.12;
+
+// https://github.com/makerdao/dss-chain-log
+interface ChainlogAbstract {
+    function wards(address) external view returns (uint256);
+    function rely(address) external;
+    function deny(address) external;
+    function keys() external view returns (bytes32[] memory);
+    function version() external view returns (string memory);
+    function ipfs() external view returns (string memory);
+    function setVersion(string calldata) external;
+    function setSha256sum(string calldata) external;
+    function setIPFS(string calldata) external;
+    function setAddress(bytes32,address) external;
+    function removeAddress(bytes32) external;
+    function count() external view returns (uint256);
+    function get(uint256) external view returns (bytes32,address);
+    function list() external view returns (bytes32[] memory);
+    function getAddress(bytes32) external view returns (address);
+}
