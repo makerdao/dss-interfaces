5776650af1368b63c82cb58e08b7f31714c08390
Brian McMichael <brian@brianmcmichael.com>
2020-11-12 16:48:16 -0500

    Add ChainlogHelper for making address accessible

--- src/dss/ChainlogAbstract.sol
+++ src/dss/ChainlogAbstract.sol
@@ -18,3 +18,14 @@ interface ChainlogAbstract {
     function list() external view returns (bytes32[] memory);
     function getAddress(bytes32) external view returns (address);
 }
+
+// Helper function for returning address or abstract of Chainlog
+//  Valid on Mainnet, Kovan, Rinkeby, Ropsten, and Goerli
+contract ChainlogHelper {
+    function Address() external pure returns (address) {
+        return 0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F;
+    }
+    function Abstract() external pure returns (ChainlogAbstract) {
+        return ChainlogAbstract(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
+    }
+}
