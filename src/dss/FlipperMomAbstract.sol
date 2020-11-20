f30f7f4329525f9676d7c9c8bbc2b29fdbf3960e
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-11-20 14:26:48 -0300

    Add authority() and owner() to DSPauseAbstract and fix modifier in FlipperMom functions

--- src/dss/FlipperMomAbstract.sol
+++ src/dss/FlipperMomAbstract.sol
@@ -2,9 +2,9 @@ pragma solidity >=0.5.12;
 
 // https://github.com/makerdao/flipper-mom/blob/master/src/FlipperMom.sol
 interface FlipperMomAbstract {
-    function owner() external returns (address);
+    function owner() external view returns (address);
+    function authority() external view returns (address);
     function setOwner(address) external;
-    function authority() external returns (address);
     function setAuthority(address) external;
     function cat() external returns (address);
     function rely(address) external;
