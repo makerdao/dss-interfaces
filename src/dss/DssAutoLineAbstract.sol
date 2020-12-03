238af6c64941858f2412fec14d829a890af3b3a7
Brian McMichael <brian@brianmcmichael.com>
2020-12-03 14:36:27 -0500

    Add DssAutoLineAbstract

---
+++ src/dss/DssAutoLineAbstract.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.5.12;
+
+// https://github.com/makerdao/dss-auto-line/blob/master/src/DssAutoLine.sol
+interface DssAutoLineAbstract {
+    function wards(address) external view returns (uint256);
+    function rely(address) external;
+    function deny(address) external;
+    function ilks(bytes32) external view returns (uint256,uint256,uint48,uint48,uint48);
+    function setIlk(bytes32,uint256,uint256,uint256) external;
+    function remIlk(bytes32) external;
+    function exec(bytes32) external returns (uint256);
+}
