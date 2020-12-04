32b99103184a46c12d67e101c7be7b7d946432c3
Brian L. McMichael <brian@brianmcmichael.com>
2020-12-03 22:39:21 -0500

    Add DssAutoLineAbstract (#39)
    * Add DssAutoLineAbstract
    
    * Add vat view

---
+++ src/dss/DssAutoLineAbstract.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.5.12;
+
+// https://github.com/makerdao/dss-auto-line/blob/master/src/DssAutoLine.sol
+interface DssAutoLineAbstract {
+    function wards(address) external view returns (uint256);
+    function rely(address) external;
+    function deny(address) external;
+    function vat() external view returns (address);
+    function ilks(bytes32) external view returns (uint256,uint256,uint48,uint48,uint48);
+    function setIlk(bytes32,uint256,uint256,uint256) external;
+    function remIlk(bytes32) external;
+    function exec(bytes32) external returns (uint256);
+}
