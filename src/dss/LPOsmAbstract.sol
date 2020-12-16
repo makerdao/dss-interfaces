007769e32844600506dfc39685550c0960646747
Lucas <lucas@makerdao.com>
2020-12-16 15:18:31 -0500

    feat: add LPOsmAbstrat

--- src/dss/LPOsmAbstract.sol
+++ src/dss/LPOsmAbstract.sol
@@ -1,23 +1,27 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.5.12;
 
-// https://github.com/makerdao/osm
-interface OsmAbstract {
+// https://github.com/makerdao/univ2-lp-oracle
+interface LPOsmAbstract {
     function wards(address) external view returns (uint256);
     function rely(address) external;
     function deny(address) external;
     function stopped() external view returns (uint256);
+    function bud(address) external view returns (uint256);
+    function dec0() external view returns (uint8);
+    function dec1() external view returns (uint8);
+    function orb0() external view returns (address);
+    function orb1() external view returns (address);
+    function wat() external view returns (bytes32);
+    function hop() external view returns (uint32);
     function src() external view returns (address);
-    function hop() external view returns (uint16);
     function zzz() external view returns (uint64);
     function cur() external view returns (uint128, uint128);
     function nxt() external view returns (uint128, uint128);
-    function bud(address) external view returns (uint256);
+    function change(address) external;
+    function step(uint256) external;
     function stop() external;
     function start() external;
-    function change(address) external;
-    function step(uint16) external;
-    function void() external;
     function pass() external view returns (bool);
     function poke() external;
     function peek() external view returns (bytes32, bool);
@@ -27,4 +31,5 @@ interface OsmAbstract {
     function diss(address) external;
     function kiss(address[] calldata) external;
     function diss(address[] calldata) external;
+    function link(uint256, address) external;
 }
