beb4c62bd3bfa7d31b2d11330f001c458550889a
Sam MacPherson <xylom44@gmail.com>
2021-02-15 13:58:40 -0500

    update to newest interface along with repo update

--- src/utils/LerpAbstract.sol
+++ src/utils/LerpAbstract.sol
@@ -1,16 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.5.12;
 
-// https://github.com/bellwoodstudios/dss-lerp/blob/master/src/Lerp.sol
+// https://github.com/makerdao/dss-lerp/blob/master/src/Lerp.sol
 interface LerpAbstract {
-    function wards(address) external view returns (uint256);
-    function rely(address) external;
-    function deny(address) external;
     function target() external view returns (address);
     function what() external view returns (bytes32);
     function start() external view returns (uint256);
     function end() external view returns (uint256);
     function duration() external view returns (uint256);
-    function init() external;
     function tick() external;
+    function wipe() external;
 }
