9fe2ba577f01bdb02d1251537bf116f00886a6bf
Brian McMichael <brian@brianmcmichael.com>
2021-04-02 17:18:00 -0400

    Updates for liquidations 2.0

--- src/dss/IlkRegistryAbstract.sol
+++ src/dss/IlkRegistryAbstract.sol
@@ -7,10 +7,11 @@ interface IlkRegistryAbstract {
     function rely(address) external;
     function deny(address) external;
     function vat() external view returns (address);
+    function dog() external view returns (address);
     function cat() external view returns (address);
     function spot() external view returns (address);
     function ilkData(bytes32) external view returns (
-        uint256, address, address, address, address, uint256, string memory, string memory
+        uint96, address, address, uint8, uint96, address, address, string memory, string memory
     );
     function ilks() external view returns (bytes32[] memory);
     function ilks(uint) external view returns (bytes32);
@@ -27,14 +28,16 @@ interface IlkRegistryAbstract {
     function list(uint256, uint256) external view returns (bytes32[] memory);
     function get(uint256) external view returns (bytes32);
     function info(bytes32) external view returns (
-        string memory, string memory, uint256, address, address, address, address
+        string memory, string memory, uint256, uint256, address, address, address, address
     );
     function pos(bytes32) external view returns (uint256);
+    function class(bytes32) external view returns (uint256);
     function gem(bytes32) external view returns (address);
     function pip(bytes32) external view returns (address);
     function join(bytes32) external view returns (address);
-    function flip(bytes32) external view returns (address);
+    function xlip(bytes32) external view returns (address);
     function dec(bytes32) external view returns (uint256);
     function symbol(bytes32) external view returns (string memory);
     function name(bytes32) external view returns (string memory);
+    function put(bytes32, address, address, uint256, uint256, address, address, string calldata, string calldata) external;
 }
