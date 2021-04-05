de66d26faa6ad56822cb8ad0ef158880d38d1d8a
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-04-05 17:29:41 -0300

    Upgrade ESMAbstract + changing licensing

--- src/dapp/DSRuneAbstract.sol
+++ src/dapp/DSRuneAbstract.sol
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity >=0.5.12;
 
 // https://github.com/makerdao/dss-spellbook
