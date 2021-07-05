2151a4ec94a23bd2ad9bebd1d44eb0ef9f9b3523
Brian McMichael <brian@brianmcmichael.com>
2021-07-05 15:55:18 -0400

    alpha imports

--- src/Interfaces.sol
+++ src/Interfaces.sol
@@ -10,18 +10,25 @@ import { DSPauseProxyAbstract } from "./dapp/DSPauseProxyAbstract.sol";
 import { DSRolesAbstract } from "./dapp/DSRolesAbstract.sol";
 import { DSSpellAbstract } from "./dapp/DSSpellAbstract.sol";
 import { DSRuneAbstract } from "./dapp/DSRuneAbstract.sol";
+import { DSThingAbstract } from "./dapp/DSThingAbstract.sol";
 import { DSTokenAbstract } from "./dapp/DSTokenAbstract.sol";
 import { DSValueAbstract } from "./dapp/DSValueAbstract.sol";
 
 import { AuthGemJoinAbstract } from "./dss/AuthGemJoinAbstract.sol";
 import { CatAbstract } from "./dss/CatAbstract.sol";
+import { ChainlogAbstract } from "./dss/ChainlogAbstract.sol";
+import { ChainlogHelper } from "./dss/ChainlogAbstract.sol";
+import { ClipAbstract } from "./dss/ClipAbstract.sol";
+import { ClipperMomAbstract } from "./dss/ClipperMomAbstract.sol";
 import { DaiAbstract } from "./dss/DaiAbstract.sol";
 import { DaiJoinAbstract } from "./dss/DaiJoinAbstract.sol";
+import { DogAbstract } from "./dss/DogAbstract.sol";
 import { DssAutoLineAbstract } from "./dss/DssAutoLineAbstract.sol";
 import { DssCdpManagerAbstract } from "./dss/DssCdpManager.sol";
 import { EndAbstract } from "./dss/EndAbstract.sol";
 import { ESMAbstract } from "./dss/ESMAbstract.sol";
 import { ETHJoinAbstract } from "./dss/ETHJoinAbstract.sol";
+import { ExponentialDecreaseAbstract } from "./dss/ExponentialDecreaseAbstract.sol";
 import { FaucetAbstract } from "./dss/FaucetAbstract.sol";
 import { FlapAbstract } from "./dss/FlapAbstract.sol";
 import { FlashAbstract } from "./dss/FlashAbstract.sol";
@@ -32,37 +39,23 @@ import { GemJoinAbstract } from "./dss/GemJoinAbstract.sol";
 import { GetCdpsAbstract } from "./dss/GetCdpsAbstract.sol";
 import { IlkRegistryAbstract } from "./dss/IlkRegistryAbstract.sol";
 import { JugAbstract } from "./dss/JugAbstract.sol";
+import { LinearDecreaseAbstract } from "./dss/LinearDecreaseAbstract.sol";
 import { LPOsmAbstract } from "./dss/LPOsmAbstract.sol";
 import { MkrAuthorityAbstract } from "./dss/MkrAuthorityAbstract.sol";
 import { MedianAbstract } from "./dss/MedianAbstract.sol";
 import { OsmAbstract } from "./dss/OsmAbstract.sol";
 import { OsmMomAbstract } from "./dss/OsmMomAbstract.sol";
 import { PotAbstract } from "./dss/PotAbstract.sol";
+import { PotHelper } from "./dss/PotHelper.sol";
 import { SpotAbstract } from "./dss/SpotAbstract.sol";
+import { StairstepExponentialDecreaseAbstract } from "./dss/StairstepExponentialDecreaseAbstract.sol";
 import { VatAbstract } from "./dss/VatAbstract.sol";
 import { VestAbstract } from "./dss/VestAbstract.sol";
 import { VowAbstract } from "./dss/VowAbstract.sol";
-import { ChainlogAbstract } from "./dss/ChainlogAbstract.sol";
-import { ChainlogHelper } from "./dss/ChainlogAbstract.sol";
 
-// Liq 2.0
-import { DogAbstract } from "./dss/DogAbstract.sol";
-import { ClipAbstract } from "./dss/ClipAbstract.sol";
-import { ClipperMomAbstract } from "./dss/ClipperMomAbstract.sol";
-import { LinearDecreaseAbstract } from "./dss/LinearDecreaseAbstract.sol";
-import {
-    StairstepExponentialDecreaseAbstract
-} from "./dss/StairstepExponentialDecreaseAbstract.sol";
-import {
-    ExponentialDecreaseAbstract
-} from "./dss/ExponentialDecreaseAbstract.sol";
-
-// Sai
 import { GemPitAbstract } from "./sai/GemPitAbstract.sol";
 import { SaiMomAbstract } from "./sai/SaiMomAbstract.sol";
 import { SaiTapAbstract } from "./sai/SaiTapAbstract.sol";
 import { SaiTopAbstract } from "./sai/SaiTopAbstract.sol";
 import { SaiTubAbstract } from "./sai/SaiTubAbstract.sol";
 import { SaiVoxAbstract } from "./sai/SaiVoxAbstract.sol";
-
-import { PotHelper } from "./dss/PotHelper.sol";
