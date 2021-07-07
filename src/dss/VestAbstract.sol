ca6350bc104192822fdda3f6974d09c609b099a5
Brian L. McMichael <brian@brianmcmichael.com>
2021-07-07 18:49:40 -0400

    Add interfaces for dss-vest and dss-flash (#55)
    * Add dss-flash interface
    
    * rm compiler warnings
    
    * add dss-vest abstract
    
    * properties are view
    
    * alpha imports

---
+++ src/dss/VestAbstract.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity >=0.5.12;
+
+// https://github.com/makerdao/dss-vest/blob/master/src/DssVest.sol
+interface VestAbstract {
+    function TWENTY_YEARS() external view returns (uint256);
+    function wards(address) external view returns (uint256);
+    function rely(address) external;
+    function deny(address) external;
+    function awards(uint256) external view returns (address, uint48, uint48, uint48, uint128, uint128, address);
+    function ids() external view returns (uint256);
+    function restricted(uint256) external view returns (uint256);
+    function cap() external view returns (uint256);
+    function file(bytes32, uint256) external;
+    function init(address, uint256, uint256, uint256, uint256, address) external returns (uint256);
+    function vest(uint256) external;
+    function vest(uint256, uint256) external;
+    function accrued(uint256) external view returns (uint256);
+    function unpaid(uint256) external view returns (uint256);
+    function restrict(uint256) external;
+    function unrestrict(uint256) external;
+    function yank(uint256) external;
+    function yank(uint256, uint256) external;
+    function move(uint256, address) external;
+    function valid(uint256) external view returns (bool);
+}
