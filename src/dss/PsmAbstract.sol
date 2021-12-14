958bce29a8d0ab775761db98183eb4ae6990609a
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2021-12-13 17:17:28 -0700

    add PSM abstract

---
+++ src/dss/PsmAbstract.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity >=0.5.12;
+
+// https://github.com/makerdao/dss-psm/src/psm.sol
+interface PsmAbstract {
+    function wards(address) external returns (uint256);
+    function rely(address) external;
+    function deny(address) external;
+    function vat() external returns (address);
+    function gemJoin() external returns (address);
+    function dai() external returns (address);
+    function daiJoin() external returns (address);
+    function ilk() external returns (bytes32);
+    function vow() external returns (address);
+    function tin() external returns (uint256);
+    function tout() external returns (uint256);
+    function file(bytes32 what, uint256 data) external;
+    function hope(address) external;
+    function nope(address) external;
+    function sellGem(address usr, uint256 gemAmt) external;
+    function buyGem(address usr, uint256 gemAmt) external;
+}
