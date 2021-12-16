65027741d70eac4c4a31218a81a202a46c586071
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2021-12-16 11:06:39 -0700

    Update src/dss/PsmAbstract.sol
    Co-authored-by: Nazzareno Massari <nazzareno@nazzarenomassari.com>

--- src/dss/PsmAbstract.sol
+++ src/dss/PsmAbstract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity >=0.5.12;
 
-// https://github.com/makerdao/dss-psm/src/psm.sol
+// https://github.com/makerdao/dss-psm/blob/master/src/psm.sol
 interface PsmAbstract {
     function wards(address) external view returns (uint256);
     function rely(address) external;
