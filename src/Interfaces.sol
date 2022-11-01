9bfd7afadd1f8c217ef05850b2555691786286cb
Henrique Barcelos <16565602+hbarcelos@users.noreply.github.com>
2022-11-01 15:27:24 -0300

    Add MIP21 contracts interfaces (#75)
    * feat: add MIP21 contracts interfaces
    
    * fix: RwaLiquidationOracle.cull signature
    
    Co-authored-by: Christopher Mooney <chris@dod.net>
    
    * feat: add mip21/RwaTokenFactory interface
    
    * chore: update reference component repos
    
    * feat: add new conduit versions
    
    * fix: add new conduit versions to the main Interfaces.sol file
    
    * fix: remove param names from RwaTokenFactoryAbstract interface
    
    Co-authored-by: Christopher Mooney <chris@dod.net>
    Co-authored-by: amusingaxl <112016538+amusingaxl@users.noreply.github.com>

--- src/Interfaces.sol
+++ src/Interfaces.sol
@@ -58,6 +58,23 @@ import { VatAbstract } from "./dss/VatAbstract.sol";
 import { VestAbstract } from "./dss/VestAbstract.sol";
 import { VowAbstract } from "./dss/VowAbstract.sol";
 
+// MIP21 Abstracts
+import {
+  RwaInputConduitBaseAbstract,
+  RwaInputConduitAbstract,
+  RwaInputConduit2Abstract,
+  RwaInputConduit3Abstract
+} from "./dss/mip21/RwaInputConduitAbstract.sol";
+import { RwaJarAbstract } from "./dss/mip21/RwaJarAbstract.sol";
+import { RwaLiquidationOracleAbstract } from "./dss/mip21/RwaLiquidationOracleAbstract.sol";
+import {
+  RwaOutputConduitBaseAbstract,
+  RwaOutputConduitAbstract,
+  RwaOutputConduit2Abstract,
+  RwaOutputConduit3Abstract
+} from "./dss/mip21/RwaOutputConduitAbstract.sol";
+import { RwaUrnAbstract } from "./dss/mip21/RwaUrnAbstract.sol";
+
 import { GemPitAbstract } from "./sai/GemPitAbstract.sol";
 import { SaiMomAbstract } from "./sai/SaiMomAbstract.sol";
 import { SaiTapAbstract } from "./sai/SaiTapAbstract.sol";
